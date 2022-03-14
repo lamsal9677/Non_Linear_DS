979c03de80dff269c35a06784c5524342c9c8964
Sanskar Lamsal <lamsal9677@yahoo.com>
2022-03-13 21:23:58 -0400

    Homework 6 Init

---
+++ Homeworks/Homework 6/IndexedBST.cpp
@@ -0,0 +1,61 @@
+#include "IndexedBST.h"
+
+IndexedBST::~IndexedBST() {
+    deleteTree(root);
+}
+
+void IndexedBST::deleteTree(Node* node) {
+    // Recursively remove all nodes in the BST (used by the destructor). 
+    // By doing this recursively, we start removing nodes from the bottom
+    // of the tree (leaf nodes), which is most efficiently because it does
+    // not require replacing any nodes.
+
+    if (node == nullptr)
+        return;
+
+    deleteTree(node->left);
+    deleteTree(node->right);
+    delete node;
+}
+
+Node* IndexedBST::search(double desiredKey) {
+    Node* cur = root;
+    while (cur != nullptr) {
+        // Return the node if the key matches
+        if (cur->key == desiredKey) {
+            return cur;
+        }
+
+        // Navigate to the left if the search key is 
+        // less than the node's key.
+        else if (desiredKey < cur->key) {
+            cur = cur->left;
+        }
+
+        // Navigate to the right if the search key is 
+        // greater than the node's key.
+        else {
+            cur = cur->right;
+        }
+    }
+
+    // The key was not found in the tree.
+    return nullptr;
+}
+
+Node* IndexedBST::at(int index) {
+    Node* cur = root;
+    while (cur != nullptr) {
+        if (index == cur->leftSize)
+            return cur;     // Found
+        else if (index < cur->leftSize)
+            cur = cur->left;    // index is in left subtree
+        else {
+            // index is in right subtree
+            index = index - cur->leftSize - 1;
+            cur = cur->right;
+        }
+    }
+
+    return nullptr;
+}
