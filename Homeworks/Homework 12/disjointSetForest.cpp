298f12407583f40aa0a861ff51d8494a981947e1
Sanskar Lamsal <lamsal9677@yahoo.com>
2022-04-25 23:16:34 -0400

    True True case working

--- Homeworks/Homework 12/disjointSetForest.cpp
+++ Homeworks/Homework 12/disjointSetForest.cpp
@@ -1,8 +1,6 @@
 #include <vector>
 #include <iostream>
 #include <string>
-#include <sstream>
-
 
 class Node {
 public:
@@ -18,25 +16,39 @@ void unionSet(Node* element1, Node* element2, bool unionByRank,bool pathCompress
 void Link(Node* element1, Node* element2);
 void printAllElements(std::vector<Node*> elements);
 
+
 void makeSet(Node* element){
 	element->parent = element;
 	element->rank = 0;
 }
 
+//implemented path compression
 Node* findSet(Node* element, bool pathCompression) {
-	if (pathCompression) {
-		if (element != element->parent) {
-			element->parent = findSet(element->parent, pathCompression);
+	if (element != element->parent) {
+		if (pathCompression) {
+		element->parent = findSet(element->parent, pathCompression);
+		return element->parent;
+		}
+		else {
+			findSet(element->parent, pathCompression);
+			element = element->parent;
+			//return element->parent;
 		}
 	}
-	else {
-		findSet(element->parent, pathCompression);
-	}
+	element = element->parent;
 	return element->parent;
 }
 
+
 void unionSet(Node* element1, Node* element2, bool unionByRank, bool pathCompression) {
-	Link(findSet(element1, false), findSet(element2, false));
+	
+	Link(findSet(element1, pathCompression), findSet(element2, pathCompression));
+	//if (!unionByRank) {
+	//	findSet(element1, pathCompression)->parent = findSet(element2, pathCompression)->parent;
+	//}
+	//else {
+	//	Link(findSet(element1, pathCompression), findSet(element2, pathCompression));
+	//}
 }
 
 void Link(Node* element1, Node* element2) {
@@ -45,11 +57,12 @@ void Link(Node* element1, Node* element2) {
 	}
 	else {
 		element1->parent = element2;
-		if (element1->rank = element2->rank) {
+		if (element1->rank == element2->rank) {
 			element2->rank = element2->rank + 1;
 		}
 	}
 }
+
 
 void printAllElements(std::vector<Node*> elements) {
 	std::cout << "Element  Parent  Set Representative" << std::endl;
@@ -60,10 +73,9 @@ void printAllElements(std::vector<Node*> elements) {
 	}
 }
 
-
 int main() {
 
-	bool unionByRank = true, pathCompression = false;
+	bool unionByRank = true, pathCompression = true;
 	int numElements = 5;
 	std::vector<Node*> elements(numElements);
 	for (int i = 0; i < numElements; i++)
@@ -74,6 +86,7 @@ int main() {
 	}
 	printAllElements(elements);
 
+	
 	unionSet(elements.at(0), elements.at(1), unionByRank, pathCompression);
 	printAllElements(elements);
 	unionSet(elements.at(0), elements.at(2), unionByRank, pathCompression);
@@ -82,35 +95,6 @@ int main() {
 	printAllElements(elements);
 	unionSet(elements.at(0), elements.at(4), unionByRank, pathCompression);
 	printAllElements(elements);
-
+	
 	return 0;
-}
-
-
-
-
-void avc() {
-	bool unionByRank = false;
-	bool pathCompression = false;
-
-	//getting user input
-	/*
-	std::cout << "Union By Rank?";
-	std::cin >> unionByRank;
-	std::cout << std::endl;
-	std::cout << "Union By Rank?";
-	std::cin >> unionByRank;
-	std::cout << std::endl;
-	*/
-
-	std::string userInput = "1 1 1";
-	//std::cout << "Element Parent Set_Representative" << std::endl;
-	//std::cin >> userInput;
-
-	std::istringstream iss(userInput);
-	std::string a, b, c;
-	iss >> a >> b >> c;
-	int num1 = stoi(a);
-	int num2 = stoi(b);
-	int num3 = stoi(c);
 }
